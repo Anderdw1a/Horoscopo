c4e776851980f081bb77fb520dc55065052441b7
Anderdw1a <andergil.j@gmail.com>
2025-10-24 12:04:54 +0200

    imagenes

--- front-page.php
+++ front-page.php
@@ -1,4 +1,14 @@
 <?php get_header(); ?>
 <main class="main">
+
+    <section class="hero">
+        <img class="img__front"
+            src="https://imgs.search.brave.com/YIHoduMqUIN9D6LZf9mTjHaWIsRB7GIaDAAnFp12Iz4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/dmVjdG9yLXByZW1p/dW0vc2lnbm9zLXpv/ZGlhY2FsZXMtYXN0/cm9sb2dpY29zLWNp/cmN1bG8tbWlzdGlj/by1jaWVsby1ub2N0/dXJuby1pbHVzdHJh/Y2lvbi1ob3Jvc2Nv/cG8tdmVjdG9yXzQ4/NDcyMC0zODU1Lmpw/Zz9zZW10PWFpc19o/eWJyaWQmdz03NDAm/cT04MA"
+            alt="Foto de los horoscopos">
+
+    </section>
+    <section class="grid__horoscopo">
+
+    </section>
 </main>
 <?php get_footer(); ?>
